535fbe42927e2ab4fb31b8da52a6cbd7c6b7022c
ARBIYA SULTANA <115937326+sultanaarbiya@users.noreply.github.com>
2024-01-03 16:30:42 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Hero from "./components/Hero.js";
+import Navigate from "./components/Task.js";
+function App() {
+  return (
+  <div>
+    <Navigate/>
+    <Hero/>
+  </div>
+  );
+}
+
+export default App;
---
+++ Task.js
@@ -0,0 +1,17 @@
+export default function  Navigate()
+{
+    return (
+<nav className="container">
+  <div className="logo">
+    <img src="/images/brand_logo.png" alt="logo_image"></img>
+  </div>
+  <ul>
+    <li href="#"><b>Menu</b></li>
+    <li href="#"><b>Location</b></li>
+    <li href="#"><b>About</b></li>
+    <li href="#"><b>Contact</b></li>
+    </ul>
+    <button >login</button>
+</nav>
+    );
+};
